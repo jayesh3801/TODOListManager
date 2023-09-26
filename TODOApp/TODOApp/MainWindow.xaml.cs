f9177afc2718f96c5c960e35aab817a5bc9885c0
jayesh3801 <61326679+jayesh3801@users.noreply.github.com>
2023-09-26 11:35:02 +0530

    First Commit

---
+++ TODOApp/TODOApp/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TODOApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddTODOButton_Click(object sender, RoutedEventArgs e)
+        {
+            string todoText = TODOInput.Text;
+            if(!string.IsNullOrEmpty(todoText) )
+            {
+                TextBlock todoTextBlock = new TextBlock
+                {
+                    Text = todoText,
+                    Foreground = new SolidColorBrush(Colors.White),
+                    Margin = new Thickness(10)
+                };
+
+                TODOList.Children.Add(todoTextBlock);
+                TODOInput.Clear();
+            }
+        }
+    }
+}
